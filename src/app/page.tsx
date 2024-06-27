5da5f7e0761ba2ff2b2b71f0516e5f8fa0081bf0
antoniomattar <antoniomattar123@icloud.com>
2024-06-27 14:03:07 +0300

    [FIX] Updated Menus 2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const restaurants = [
   {
     resto_name: 'Porky Way',
     resto_img_url: 'logos/porky-way.png',
-    resto_menu_path: 'menus/the-grill-station-menu.pdf',
+    resto_menu_path: 'menus/the-porky-way-menu.pdf',
   },
   {
     resto_name: 'The Youth Bar',
@@ -17,12 +17,12 @@ const restaurants = [
   {
     resto_name: 'Frozen Smiles',
     resto_img_url: 'logos_webp/frozen-smiles.webp',
-    resto_menu_path: 'menus/menu-1.pdf',
+    resto_menu_path: 'menus/frozen-smiles-menu.pdf',
   },
   {
     resto_name: 'The Wrapery',
     resto_img_url: 'logos/the-wrapery.png',
-    resto_menu_path: 'menus/george-menu.pdf',
+    resto_menu_path: 'menus/the-wrapery-menu.pdf',
   },
 ];
 
